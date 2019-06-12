9d6944b26f6cd9eceab8c87cf10eaeb2c2de6a9a
Ibnu Hajar <ibnuhajar.syukur@gmail.com>
2019-06-12 17:22:03 +0700

    add start

--- index.js
+++ index.js
@@ -25,8 +25,25 @@ app.post("/webhook", (req, res) => {
 });
 
 app.get("/webhook", (req, res) => {
-    console.log(req, res);
-    res.send({status: "OK"});
+    let verify_token = process.env.VERIFICATION_TOKEN;
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    if(mode && token) {
+        if(mode === "subscribe") {
+            if (token === verify_token) {
+                console.log(" >>>> Verified !");
+                res.status(200).send(challenge);
+            } else {
+                res.sendStatus(403);
+            }
+        } else {
+            res.sendStatus(403);
+        }
+    } else {
+        res.sendStatus(403);
+    }
 })
 
 app.get("/messages/:id", async(req, res, next) => {
