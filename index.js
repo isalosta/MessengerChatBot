f07c40109c99c6bf09c70dd824d991b8cea0a37a
Ibnu Hajar <ibnuhajar.syukur@gmail.com>
2019-06-12 16:41:41 +0700

    Update Temp 1

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const Express = require('express');
+const BodyParser = require('body-parser');
+const Mongoose = require('mongoose');
+const DotEnv = require('dotenv');
+const Request = require('request');
+
+DotEnv.config({path: `${__dirname}/.env`});
+
+const app = Express();
+var db = Mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
+
+
+app.use(BodyParser.json());
+
+app.post("/webhook", (req, res) => {
+    console.log(req);
+});
+
+app.get("/webhook", (req, res) => {
+    console.log(req, res);
+})
+
+app.get("/messages/:id", async(req, res, next) => {
+    console.log(req, res);
+});
+
+app.get("/messages/delete/:id", async(req, res, next) => {
+    console.log(req, res);
+})
+
+app.listen(process.env.port || 8080, (port) => {console.log(`Server RUN on ${port}`)});
