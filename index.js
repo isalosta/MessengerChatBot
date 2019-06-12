e36688350db7819493c6ee53140715c51f57d97a
Ibnu Hajar <ibnuhajar.syukur@gmail.com>
2019-06-13 03:48:04 +0700

    response

--- index.js
+++ index.js
@@ -41,29 +41,31 @@ app.post("/webhook", (req, res) => {
             if(res.length > 0) {
                 let dt = res[0];
                 let step = parseInt(dt.step);
-                if(dt.is_not_first && step < 1) {
+                if(dt.is_not_first && step === 0) {
                     UserModel.update({user_id: body.from.id}, {
                         user_id: body.from.id,
                         user_name: body.from.name,
                         step: "1",
                         is_not_first: dt.is_not_first ? true : false
+                    }).then(res_ => {
+                        PostBack(process.env.ACCESS_TOKEN, body.from.id, `Welcome Again ${body.from.name}`).then(res => {
+                            console.log("SENT! >>> ", body.from.id);
+                        }).catch(err_ => {console.log("SENDING FAIL!")});
                     });
-                    PostBack(process.env.ACCESS_TOKEN, body.from.id, `Welcome Again ${body.from.name}`).then(res => {
-                        console.log("SENT! >>> ", body.from.id);
-                    }).catch(err => {console.log("SENDING FAIL!")});
                 }
                 if(step < 3) {
+                    step = step + 1;
                     UserModel.update({user_id: body.from.id}, {
                         user_id: body.from.id,
                         user_name: body.from.name,
-                        step: (step + 1).toString(),
+                        step: `${step}`,
                         is_not_first: dt.is_not_first ? true : false
+                    }).then(res_ => {
+                        PostBack(process.env.ACCESS_TOKEN, body.from.id, Verbal.Question[dt.step].text).then(res => {
+                            console.log("SENT! >>> ", body.from.id);
+                        }).catch(err_ => {console.log("SENDING FAIL!")});
                     });
-                    PostBack(process.env.ACCESS_TOKEN, body.from.id, Verbal.Question[dt.step].text).then(res => {
-                        console.log("SENT! >>> ", body.from.id);
-                    }).catch(err => {console.log("SENDING FAIL!")});
                 } else {
-
                     PostBack(process.env.ACCESS_TOKEN, body.from.id, Verbal.EndResponse("XXX")[body.text.indexOf("y") > 1 ? "y" : "n"].text).then(res => {
                         console.log("SENT! >>> ", body.from.id);
                         UserModel.update({user_id: body.from.id}, {
